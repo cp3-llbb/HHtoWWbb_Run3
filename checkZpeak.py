719b0afc9e916e84a3ebd4251592f4d223f56af7
Oguz Guzel <44399848+Oguz-Guzel@users.noreply.github.com>
2023-01-16 17:38:12 +0100

    Update checkZpeak.py

--- checkZpeak.py
+++ checkZpeak.py
@@ -98,11 +98,11 @@ class NanoBaseHHWWbb(NanoAODModule, HistogramsModule):
         muons = op.sort(op.select(tree.Muon, lambda mu: op.AND(
             mu.pt >= 5.,
             op.abs(mu.eta) <= 2.4,
-            # op.abs(mu.dxy) <= 0.05,
-            # op.abs(mu.dz) <= 0.1,
-            # mu.miniPFRelIso_all <= 0.4,
-            # mu.sip3d <= 8,
-            # mu.tightId
+            op.abs(mu.dxy) <= 0.05,
+            op.abs(mu.dz) <= 0.1,
+            mu.miniPFRelIso_all <= 0.4,
+            mu.sip3d <= 8,
+            mu.tightId
         )), lambda mu: -mu.pt)
         #############################################################################
         #                                 Electrons                                 #
