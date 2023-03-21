e013f09c2289f82542eb75be5c5bcc641a02fddd
Oguz-Guzel <44399848+Oguz-Guzel@users.noreply.github.com>
2023-03-21 13:04:46 +0100

    tidy up modules

---
+++ python/basePlotter.py
@@ -0,0 +1,67 @@
+from bamboo.analysismodules import NanoAODModule, HistogramsModule
+from bamboo.treedecorators import NanoAODDescription
+
+
+class NanoBaseHHWWbb(NanoAODModule, HistogramsModule):
+    def __init__(self, args):
+        super(NanoBaseHHWWbb, self).__init__(args)
+
+    def addArgs(self, parser):
+        super(NanoBaseHHWWbb, self).addArgs(parser)
+        parser.add_argument("--era",
+                            action='store',
+                            type=str,
+                            default=None,
+                            help='This has no use right now!')
+
+    def prepareTree(self, tree, sample=None, sampleCfg=None):
+        def isMC():
+            if sampleCfg['type'] == 'data':
+                return False
+            elif sampleCfg['type'] == 'mc':
+                return True
+            else:
+                raise RuntimeError(
+                    f"The type '{sampleCfg['type']}' of {sample} dataset not understood.")
+
+        era = sampleCfg['era']  # reserved for future use
+        self.is_MC = isMC()
+        self.triggersPerPrimaryDataset = {}
+
+        def addHLTPath(PD, HLT):
+            if PD not in self.triggersPerPrimaryDataset.keys():
+                self.triggersPerPrimaryDataset[PD] = []
+            try:
+                self.triggersPerPrimaryDataset[PD].append(
+                    getattr(tree.HLT, HLT))
+            except AttributeError:
+                print("Couldn't find branch tree.HLT.%s, will omit it!" % HLT)
+
+        def getNanoAODDescription():
+            groups = ["HLT_", "MET_"]
+            collections = ["nElectron", "nJet", "nMuon", "nFatJet", "nSubJet"]
+            varReaders = []
+            return NanoAODDescription(groups=groups, collections=collections, systVariations=varReaders)
+
+        tree, noSel, backend, lumiArgs = super(NanoBaseHHWWbb, self).prepareTree(tree=tree,
+                                                                                 sample=sample,
+                                                                                 sampleCfg=sampleCfg,
+                                                                                 description=getNanoAODDescription(),
+                                                                                 backend="lazy")
+        ### Triggers ###
+        # Muon
+        addHLTPath('Muon', 'IsoMu24')
+        addHLTPath('Muon', 'IsoMu27')
+        addHLTPath('Muon', 'Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8')
+        # EGamma
+        addHLTPath('EGamma', 'Ele32_WPTight_Gsf')
+        addHLTPath('EGamma', 'Ele23_Ele12_CaloIdL_TrackIdL_IsoVL')
+        # MuonEG
+        addHLTPath('MuonEG', 'Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ')
+        # SingleMuon
+        addHLTPath('SingleMuon', 'IsoMu24')
+        addHLTPath('SingleMuon', 'IsoMu27')
+        # DoubleMuon
+        addHLTPath('DoubleMuon', 'Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8')
+
+        return tree, noSel, backend, lumiArgs
--- python/controlPlotter.py
+++ python/controlPlotter.py
@@ -1,6 +1,5 @@
-from bamboo.analysismodules import NanoAODModule, HistogramsModule
+
 from bamboo.analysisutils import makeMultiPrimaryDatasetTriggerSelection
-from bamboo.treedecorators import NanoAODDescription
 from bamboo.plots import Plot, CutFlowReport
 from bamboo.plots import EquidistantBinning as EqBin
 from bamboo import treefunctions as op
@@ -8,70 +7,14 @@ from bamboo import treefunctions as op
 import definitions as defs
 from itertools import chain
 
+from basePlotter import NanoBaseHHWWbb
 
-class NanoBaseHHWWbb(NanoAODModule, HistogramsModule):
+
+class controlPlotter(NanoBaseHHWWbb):
+    """"""
+
     def __init__(self, args):
-        super(NanoBaseHHWWbb, self).__init__(args)
-
-    def addArgs(self, parser):
-        super(NanoBaseHHWWbb, self).addArgs(parser)
-        parser.add_argument("--era",
-                            action='store',
-                            type=str,
-                            default=None,
-                            help='This has no use right now!')
-
-    def prepareTree(self, tree, sample=None, sampleCfg=None):
-        def isMC():
-            if sampleCfg['type'] == 'data':
-                return False
-            elif sampleCfg['type'] == 'mc':
-                return True
-            else:
-                raise RuntimeError(
-                    f"The type '{sampleCfg['type']}' of {sample} dataset not understood.")
-
-        era = sampleCfg['era']  # reserved for future use
-        self.is_MC = isMC()
-        self.triggersPerPrimaryDataset = {}
-
-        def addHLTPath(PD, HLT):
-            if PD not in self.triggersPerPrimaryDataset.keys():
-                self.triggersPerPrimaryDataset[PD] = []
-            try:
-                self.triggersPerPrimaryDataset[PD].append(
-                    getattr(tree.HLT, HLT))
-            except AttributeError:
-                print("Couldn't find branch tree.HLT.%s, will omit it!" % HLT)
-
-        def getNanoAODDescription():
-            groups = ["HLT_", "MET_"]
-            collections = ["nElectron", "nJet", "nMuon", "nFatJet", "nSubJet"]
-            varReaders = []
-            return NanoAODDescription(groups=groups, collections=collections, systVariations=varReaders)
-
-        tree, noSel, backend, lumiArgs = super(NanoBaseHHWWbb, self).prepareTree(tree=tree,
-                                                                                 sample=sample,
-                                                                                 sampleCfg=sampleCfg,
-                                                                                 description=getNanoAODDescription(),
-                                                                                 backend="lazy")
-        ### Triggers ###
-        # Muon
-        addHLTPath('Muon', 'IsoMu24')
-        addHLTPath('Muon', 'IsoMu27')
-        addHLTPath('Muon', 'Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8')
-        # EGamma
-        addHLTPath('EGamma', 'Ele32_WPTight_Gsf')
-        addHLTPath('EGamma', 'Ele23_Ele12_CaloIdL_TrackIdL_IsoVL')
-        # MuonEG
-        addHLTPath('MuonEG', 'Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ')
-        # SingleMuon
-        addHLTPath('SingleMuon', 'IsoMu24')
-        addHLTPath('SingleMuon', 'IsoMu27')
-        # DoubleMuon
-        addHLTPath('DoubleMuon', 'Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8')
-
-        return tree, noSel, backend, lumiArgs
+        super(controlPlotter, self).__init__(args)
 
     def definePlots(self, tree, noSel, sample=None, sampleCfg=None):
         plots = []
